e0d85ab8b95cf62655143a8cfd51dd17e32dc72a
ARBAJ ALAM <codarbajak@gmail.com>
2023-02-09 21:27:47 +0530

    This is my first commit here 09/02/023  not full practice yet here ok

---
+++ src/PWskillPracticeCode/Operators_experssion_practice01.java
@@ -0,0 +1,27 @@
+package PWskillPracticeCode;
+import java.util.Scanner;
+public class Operators_experssion_practice01 {
+    public static void main(String[] args) {
+
+        // Question no 01
+        // Print the all Alphabet Latter from a to z ;
+
+//        for(  char a =  'a' ; a<= 'z' ; a++){
+//            System.out.println(a);
+
+        // Question no 02
+        // Print from 1 to 10
+
+//        for( int a = 1 ; a<= 10 ; a++  ){
+//            System.out.println(a);
+//        }
+
+        // Question no 03
+        // Write a program to print the value of input if it is even and divisible by 3 .
+        Scanner sc = new Scanner(System.in);
+        System.out.println( " Enter the number which you want to check if it is even and divisible by 3");
+        int num = sc.nextInt();
+
+
+    }
+}
